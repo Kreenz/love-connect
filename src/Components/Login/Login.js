18d9e14aa47a1e003c8b5d8f83768dc089eed65f
Oriol Fornos <oriol-1-2@hotmail.com>
2022-09-07 16:17:00 +0200

    Test branch
    Testing

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -13,57 +13,65 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 
+const FormWrapper = styled.div`
+  border:2px solid black;
+
+`
+
 const Login = (props) => {
 
   return (
     <Wrapper>
       <Logo/>
-      <Form 
-        name="login"
-        inputs={
-          [{
-            label:"Usuario mail: ",
-            name:"mail",
-            type:"text"
-          },
-          {
-            label:"Contraseña: ",
-            name:"passwd",
-            type:"text"
-          }]
-        }
-        /* styles={} */
-        /* submit={funtion} */
-      />
-      <Form 
-        name="login"
-        inputs={
-          [{
-            label:"Login",
-            name:"submit",
-            type:"submit",
-            fun: () => {
-              //let form = document.getElementByName("login");
-              //sacas datos y tiras luego pa alante
-              props.setUser({
-                loggedIn: true,
-                userId: "QR2LzSe7dATcyAEnuWu4",
-                username: "Oriol",
-                email:"oriol-1-2@hotmail.com",
-                age:23,
-                distance:11,
-                recent:null,
-                gender:"chico",
-                tastes:[],
-                photos:props.user.photos,
-                localitzation:{lat:41.570248, long:1.997183}
-              })
-            }
-          }]
-        }
-        /* styles={} */
-        /* submit={funtion} */
-      />
+      <FormWrapper>
+        <Form 
+          name="login"
+          inputs={
+            [{
+              label:"Usuario mail: ",
+              name:"mail",
+              type:"text"
+            },
+            {
+              label:"Contraseña: ",
+              name:"passwd",
+              type:"text"
+            }]
+          }
+          /* styles={} */
+          /* submit={funtion} */
+        />
+        <Form 
+          name="login"
+          inputs={
+            [{
+              label:"Login",
+              name:"submit",
+              type:"submit",
+              fun: () => {
+                //let form = document.getElementByName("login");
+                //sacas datos y tiras luego pa alante
+                props.setUser({
+                  loggedIn: true,
+                  userId: "QR2LzSe7dATcyAEnuWu4",
+                  username: "Oriol",
+                  email:"oriol-1-2@hotmail.com",
+                  age:23,
+                  distance:11,
+                  recent:null,
+                  gender:"chico",
+                  tastes:[],
+                  photos:props.user.photos,
+                  localitzation:{lat:41.570248, long:1.997183}
+                })
+              }
+            }]
+          }
+          /* styles={} */
+          /* submit={funtion} */
+        />
+      </FormWrapper>
+      
       <Button 
         name="Registrarse"
         boolean={true}
